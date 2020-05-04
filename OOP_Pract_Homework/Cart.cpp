60300eac573318e6d2c43717863ea9928b483666
Jordan Jordanov <50670681+ludojori@users.noreply.github.com>
2020-05-05 00:27:51 +0300

    class Cart reconfigured

--- OOP_Pract_Homework/Cart.cpp
+++ OOP_Pract_Homework/Cart.cpp
@@ -1,77 +1,40 @@
 #include "Cart.h"
 
-Cart::Cart() :_total(0.0f), _size(0), _cap(1)
+Product* productCategorizer(Product* product)
 {
-	_products = new Product * [_cap] { nullptr };
+	if (product->getCategory() == Product::Category::DEVICE)
+	{
+		Device* device = new Device;
+		device = dynamic_cast<Device*>(product);
+		return device;
+	}
+	return product;
 }
 
-Cart::Cart(const Cart& other) : _total(other._total), _size(other._size), _cap(other._cap)
+Cart::Cart() :_total(0.0f)
 {
-	_products = new Product * [_cap] { nullptr };
-	for (unsigned int i = 0; i < _size; ++i)
-	{
-		addProduct(other._products[i]);
-	}
-}
-
-Cart& Cart::operator=(const Cart& other)
-{
-	if (this != &other)
-	{
-		_total = other._total;
-		_size = other._size;
-		_cap = other._cap;
-		clear();
-		_products = new Product*[_cap];
-		for (unsigned int i = 0; i < _size; ++i)
-		{
-			_products[i] = other._products[i];
-		}
-	}
-	return *this;
 }
 
 Cart::~Cart()
 {
-	clear();
 }
 
 Cart& Cart::operator+(Product* product)
 {
-	addProduct(product);
+	add(product);
 	return *this;
 }
 
-// changes required...
-Cart& Cart::addProduct(Product* product)
+Cart& Cart::add(Product* product)
 {
-	if (_size == _cap)
-	{
-		resize();
-	}
-	if (product->getCategory() == Product::Category::UNDEFINED)
-	{
-		_products[_size - 1] = product;
-	}
-	if (product->getCategory() == Product::Category::DEVICE)
-	{
-		_products[_size - 1] = dynamic_cast<Device*>(product);
-	}
-	if (product->getCategory() == Product::Category::ACCESSORY)
-	{
-		_products[_size - 1] = dynamic_cast<Accessory*>(product);
-	}
-	if (product->getCategory() == Product::Category::BOOK)
-	{
-		_products[_size - 1] = dynamic_cast<Book*>(product);
-	}
-	incrementSize();
+	_products.add(product);
+	_total += product->getPrice();
 	return *this;
 }
 
-void Cart::printProducts() const
+void Cart::print() const
 {
-	for (unsigned int i = 0; i < _size; ++i)
+	for (unsigned int i = 0; i < _products.size(); ++i)
 	{
 		_products[i]->printInfo();
 	}
@@ -79,32 +42,8 @@ void Cart::printProducts() const
 
 void Cart::clear()
 {
-	if (_products != nullptr)
-	{
-		for (unsigned int i = 0; i < _size; ++i)
-		{
-			if (_products[i] != nullptr)
-			{
-				delete _products[i];
-			}
-		}
-		delete[] _products;
-	}
-}
-
-bool Cart::isEmpty() const
-{
-	return (_size == 0);
-}
-
-unsigned int Cart::getSize() const
-{
-	return _size;
-}
-
-unsigned int Cart::getCap() const
-{
-	return _cap;
+	_products.clear();
+	_total = 0.0f;
 }
 
 double Cart::getTotal() const
@@ -112,28 +51,7 @@ double Cart::getTotal() const
 	return _total;
 }
 
-void Cart::resize()
+bool Cart::isEmpty() const
 {
-	_cap *= 2;
-	Product** temp = new Product*[_cap];
-	for (unsigned int i = 0; i < _size; ++i)
-	{
-		temp[i] = _products[i];
-	}
-	clear();
-	_products = temp;
-}
-
-void Cart::incrementSize()
-{
-	++_size;
-}
-
-void Cart::updateTotal()
-{
-	_total = 0;
-	for (unsigned int i = 0; i < _size; ++i)
-	{
-		_total += _products[i]->getPrice();
-	}
+	return _products.isEmpty();
 }
--- OOP_Pract_Homework/Cart.h
+++ OOP_Pract_Homework/Cart.h
@@ -4,36 +4,28 @@
 #include "Book.h"
 #include "Accessory.h"
 #include "Food.h"
+#include "TemplateArray.h"
 
 class Cart
 {
 public:
 	Cart();
-	Cart(const Cart& other);
-	Cart& operator=(const Cart& other);
+	Cart(const Cart& other) = delete;
+	Cart& operator=(const Cart& other) = delete;
 	~Cart();
 
 	Cart& operator+(Product* product);
 
-	Cart& addProduct(Product* product);
-	void printProducts() const;
+	Cart& add(Product* product);
+	void print() const;
 	void clear();
 
 	double getTotal() const;
 	bool isEmpty() const;
 
 private:
-	Product** _products = nullptr;
-	unsigned int _size;
-	unsigned int _cap;
+	TemplateArray<Product*> _products;
 	double _total;
-
-	unsigned int getSize() const;
-	unsigned int getCap() const;
-
-	void resize();
-	void incrementSize();
-	void updateTotal();
 
 };
 
